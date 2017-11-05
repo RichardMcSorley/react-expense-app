4a771fa866f42676d8c7585138234a4f7187bae3
Richard McSorley <waltonproduct@gmail.com>
2017-11-05 10:26:44 -0600

    setup redux

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import {Provider} from 'react-redux';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss'
 import ReactDOM  from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import * as expenseActions from './actions/expenses';
+import * as filterActions from './actions/filters';
+import expenseSelector from './selectors/expenses';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+const store = configureStore();
+
+store.dispatch(expenseActions.addExpense({description: 'Water Bill', amount: 4500}));
+store.dispatch(expenseActions.addExpense({description: 'Gas Bill', createdAt: 1000}));
+store.dispatch(expenseActions.addExpense({description: 'Rent', amount: 109400}));
+
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
