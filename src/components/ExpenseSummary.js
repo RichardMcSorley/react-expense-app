6d9b25fa5ba1433ad05650c84c1c980bb1f36e93
Richard McSorley <waltonproduct@gmail.com>
2017-11-06 12:58:01 -0600

    added feature ExpenseSummary

---
+++ src/components/ExpenseSummary.js
@@ -0,0 +1,25 @@
+import numeral from 'numeral';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import getTotals from '../selectors/totals';
+
+export class ExpenseSummary extends Component {
+    
+    render() {
+        const expenseWord = this.props.expenseCount === 1 ? 'expense' : 'expenses';
+        const formatTotal = numeral(this.props.expensesTotal / 100).format('$0,0.00');
+        return (
+            <div>
+                <h1>Viewing {this.props.expenseCount} {expenseWord} totalling {formatTotal}</h1>
+            </div>
+            );
+    };
+};
+
+const mapStateToProps = (state) =>{
+    return {
+        expenseCount: state.expenses.length,
+        expensesTotal: getTotals(state.expenses)
+    };
+};
+export default connect(mapStateToProps)(ExpenseSummary)
---
+++ src/tests/components/ExpenseSummary.test.js
@@ -0,0 +1,26 @@
+import {ExpenseSummary} from './../../components/ExpenseSummary';
+import React from 'react';
+import { shallow } from 'enzyme';
+import expenses from '../fixtures/expenses';
+import getTotals from '../../selectors/totals';
+
+let expenseCount, expensesTotal, wrapper
+
+beforeEach(()=>{
+    expenseCount = expenses.length;
+    expensesTotal = getTotals(expenses);
+    wrapper = shallow(<ExpenseSummary expenseCount={expenseCount} expensesTotal={expensesTotal}/>);
+});
+
+
+test('should render ExpenseSummary with multple expenses', () => {
+    expect(wrapper).toMatchSnapshot();
+})
+
+test('should render ExpenseSummary with 1 expense', () => {
+    wrapper.setProps({
+        expenseCount: 1,
+        expensesTotal: getTotals([expenses[0]])
+    })
+    expect(wrapper).toMatchSnapshot();
+})
