d990b2ca537bb563abd331fd5be39f256fc3a02f
Richard McSorley <waltonproduct@gmail.com>
2017-11-06 12:54:13 -0600

    new feature expenseSummary

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpoenseListFilters from './ExpenseListFilters';
+import ExpenseSummary from './ExpenseSummary';
 
 
 const DashboardPage = () => (
     <div>
         <ExpoenseListFilters />
         <ExpenseList />
+        <ExpenseSummary />
     </div>
 );
 
